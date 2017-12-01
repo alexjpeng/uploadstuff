460e2eb82d8bfe860c0688cfa07cf6104592f3c3
alexanduh <alexander.j.peng@gmail.com>
2017-11-30 17:33:20 -0800

    live update (working???)

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const fileUpload = require("express-fileupload");
 const ls = require("ls");
 const querystring = require("querystring");
 const basicAuth = require("basic-auth");
+const cookieParser = require("cookie-parser");
 require('dotenv').config()
 
 const app = express();
 app.use(morgan("dev"));
 app.use(fileUpload());
 app.set("view engine", "pug");
+app.use(cookieParser());
 app.use("/static", express.static(path.join(__dirname, "static")));
 
 const FileDB = require("./FileDB");
@@ -22,25 +24,29 @@ const authValid = credentials => {
 	return credentials && credentials.name == process.env.AUTH_USERNAME && credentials.pass == process.env.AUTH_PASSWORD
 }
 
+const getAuthString = req => req.get('Authorization') || req.cookies.auth;
+
 const authMiddleware = () => (req, res, next) => {
-	const credentials = basicAuth(req);
+	const credentials = basicAuth.parse(getAuthString(req));
 	console.log('i got here');
 
 	if (authValid(credentials)) {
 		req.auth = credentials;
+		res.cookie("auth", getAuthString(req));	
 		next()
 	} else {
 		res.statusCode = 401
 		res.setHeader('WWW-Authenticate', 'Basic realm="example"')
-		const files = getFiles();
+		const files = getFiles();	
 		res.render("list", { files, message: "Username or password is not correct", messageType: "danger" });
 	}
 }
 
 const userMiddleware = () => (req, res, next) => {
-	const credentials = basicAuth(req);
+	const credentials = basicAuth.parse(getAuthString(req));
 	if (authValid(credentials)) {
 		req.auth = credentials;
+		res.cookie("auth", getAuthString(req));
 		next();
 	} else {
 		next();
@@ -94,6 +100,14 @@ app.get("/", userMiddleware(), (req, res) => {
 	const auth = req.auth;
 	res.set("cache-control", "private, max-age=0, no-cache, no-store")
 	res.render("list", { files, auth });
+});
+
+app.get("/viewList", userMiddleware(), (req, res) => {
+	const files = getFiles();
+	const auth = req.auth;
+	console.log(req.cookies);
+	res.set("cache-control", "private, max-age=0, no-cache, no-store")
+	res.render("includes/viewList", { files, auth });
 });
 
 app.get("/uploads/:name", (req, res) => {
